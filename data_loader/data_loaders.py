054cf00621f994b9dd2ecf789b1791951b551842
Timo Stoffregen <timo.stoffregen@outlook.com>
2020-06-07 16:35:53 +1000

    PEP8 compliant

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -2,9 +2,9 @@ from torch.utils.data import DataLoader
 # local modules
 from .dataset import DynamicH5Dataset
 
+
 class InferenceDataLoader(DataLoader):
-    """
-    """
+
     def __init__(self, data_path, num_workers=1, pin_memory=True, dataset_kwargs=None, ltype="H5"):
         if dataset_kwargs is None:
             dataset_kwargs = {}
