e465b0c214fa4d66f3d007eb112ec1e4dddd5cbb
Timo Stoffregen <timo.stoffregen@outlook.com>
2020-07-15 09:13:32 +1000

    Added flow batch processing scripts. Updated contrast max submodule

---
+++ scripts/ours/batch_inference_ours_flow_MMP.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+model="${1?"Usage: $0 <model.pth> <input/folder> <output/folder>"}"
+input_folder="${2?"Usage: $0 <model.pth> <input/folder> <output/folder>"}"
+output_base_folder="${3?"Usage: $0 <model.pth> <input/folder> <output/folder>"}"
+for input in "$input_folder"/*/; do
+    sequence_name=$(basename "$input")
+    output_folder="${output_base_folder}/${sequence_name}"
+    python "${EVENT_CNN_BASE}/inference.py" \
+        --checkpoint_path "$model" \
+        --events_file_path "${input}/memmaps" \
+        --output_folder "$output_folder" \
+        --loader_type MMP \
+        --is_flow \
+        --filter_hot_events
+done
